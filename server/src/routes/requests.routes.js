1ee114c9b83321e291773913240fe014ecb410fe
KarlaCValencia <a01738420@tec.mx>
2026-05-21 10:28:46 -0600

    practica 13: validacion con Zod

--- server/src/routes/requests.routes.js
+++ server/src/routes/requests.routes.js
@@ -1,13 +1,15 @@
 const router = require('express').Router()
 const { getAll, getById, create, update, remove } = require('../controllers/requests.controller')
 const { isAuthenticated } = require('../middleware/auth')
+const { validate } = require('../middleware/validate')
+const { createRequestSchema, updateRequestSchema } = require('../schemas/requests.schema')
 
 router.use(isAuthenticated)
 
 router.get('/', getAll)
 router.get('/:id', getById)
-router.post('/', create)
-router.put('/:id', update)
+router.post('/', validate(createRequestSchema), create)
+router.put('/:id', validate(updateRequestSchema), update)
 router.delete('/:id', remove)
 
 module.exports = router
